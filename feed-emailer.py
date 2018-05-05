6269df2050c5e0a1eabc342f889884c1030cd72d
Abitha <abithathyagarajan@gmail.com>
2018-05-05 14:35:52 +0530

    Add PGP support!

--- feed-emailer.py
+++ feed-emailer.py
@@ -17,12 +17,14 @@
 
 
 import feedparser
-import html2text
+from html2text import html2text
 import smtplib
+import gnupg
 import ruamel.yaml as yaml
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
+from time import mktime
 
 
 # open the config file
@@ -37,6 +39,14 @@ sender = smtplib.SMTP_SSL(
 sender.login(config['from'], config['password'])
 
 
+# gnupg
+gpg = gnupg.GPG(
+    binary=config['gnupg'],
+    homedir=config['gpg-home'],
+    keyring=config['keyring'],
+    secring=config['secring'])
+
+
 def create_feed_email(feed, item):
     """Creates and returns a MIME email for the feed item.
     """
@@ -48,25 +58,29 @@ def create_feed_email(feed, item):
     Email['From'] = config['from']
     Email['To'] = config['to']
 
-    email_text = (item['title'] + "\n"
+    email_text = str(gpg.encrypt(item['title'] + "\n"
         + item['link'] + "\n"
         + item['published'] + "\n\n"
-        + item['summary'] + "\n\n"
-        + "This post appeared first on [" + feed['channel']['title'] + "]("
-        + feed['channel']['link'] + ").")
+        + html2text(item['summary']) + "\n\n", # message content
+        config['recipient-key'],
+        default_key=config['sender-key'],
+        passphrase=config['passphrase'],
+        encrypt=True
+    ))
 
-    email_html = ("<h2><a href='" + item['link'] + "'>"
+    """email_html = ("<h2><a href='" + item['link'] + "'>"
         + item['title'] + "</a></h2>"
         + "<span>" + item['published'] + "</span><p>"
         + item['summary']
         + "</p><p>This post appeared first on "
         + "<a href='" + feed['channel']['link'] + "'>"
-        + feed['channel']['title'] + "</a>.</p>")
+        + feed['channel']['title'] + "</a>.</p>"
+    )"""
 
     email_text = MIMEText(email_text, 'plain')
-    email_html = MIMEText(email_html, 'html')
+    #email_html = MIMEText(email_html, 'html')
     Email.attach(email_text)
-    Email.attach(email_html)
+    #Email.attach(email_html)
 
     return Email
 
@@ -80,16 +94,23 @@ def create_invalid_feed_email(feed):
     Email['From'] = config['from']
     Email['To'] = config['to']
 
-    email_text = "The feed '" + feed['href'] + \
-        "' is not a valid ATOM/RSS feed."
+    email_text = str(gpg.encrypt("The feed '" + feed['href'] + \
+        "' is not a valid ATOM/RSS feed.", # message content
+        config['recipient-key'],
+        default_key=config['sender-key'],
+        passphrase=config['passphrase'],
+        encrypt=True
+    ))
 
+    """
     email_html = "The feed <a href='" + feed['href'] + "'>" + feed['href'] + \
         "</a> is not a valid ATOM/RSS feed."
+    """
 
     email_text = MIMEText(email_text, 'plain')
-    email_html = MIMEText(email_html, 'html')
+    #email_html = MIMEText(email_html, 'html')
     Email.attach(email_text)
-    Email.attach(email_html)
+    #Email.attach(email_html)
 
     return Email
 
@@ -98,8 +119,9 @@ for feed_url in config['feeds']:
     feed = feedparser.parse(feed_url)
     if feed['bozo'] != 1:
         for item in feed['items']:
-            if item['published_parsed'] > config['last_accessed']:
+            if datetime.fromtimestamp(mktime(item['published_parsed'])) > config['last_accessed']:
                 sender.send_message(create_feed_email(feed, item))
+                #print(item['published_parsed'])
     else:
         sender.send_message(create_invalid_feed_email(feed))
 
